d7c03acd169a79773981cf6927a8848c6b30ef8d
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-13 01:41:20 +0600

    done

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateninjaDto } from './dto/create-ninja.dto';
 
 //  This is a service class that will be used to handle all the business logic of our application.
 
@@ -26,6 +27,32 @@ export class NinjasService {
     if (weapon) {
       return this.ninjas.filter((ninja) => ninja.weapon === weapon);
     }
+    return this.ninjas;
+  }
+
+  createNinja(createNinjaDto: CreateninjaDto) {
+    const newNinja = {
+      id: this.ninjas.length + 1,
+      ...createNinjaDto,
+    };
+    this.ninjas.push(newNinja);
+    return this.ninjas;
+  }
+
+  updateNinja(id: number, updateNinjaDto: CreateninjaDto) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === Number(id));
+    if (ninja) {
+      ninja.name = updateNinjaDto.name;
+      ninja.weapon = updateNinjaDto.weapon;
+    }
+    return this.ninjas;
+  }
+
+  deleteNinja(id: number) {
+    // const tobeRemoved = this.ninjas.findIndex(
+    //   (ninja) => ninja.id === Number(id),
+    // );
+    this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id);
     return this.ninjas;
   }
 }
